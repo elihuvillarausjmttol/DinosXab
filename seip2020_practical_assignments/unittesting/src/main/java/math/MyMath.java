fbbbde97739e4c41b5c44be0ffa9fd79933a9728
elihuvillarausjmttol <miaoshun8077941573@163.com>
2021-07-03 11:43:13 +0800

    In this commit i added a couple of comments for clarity and i also added the method isPrime for calculating if a given number is a prime number.

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -6,21 +6,40 @@ package math;
  * of numbers ranging from 0 to 12, using a loop (not recursion).
  */
 public class MyMath {
-	
+		
+		/*
+		 * A method that calculates factorial of numbers ranging from 0 to 12.
+		 */
 		public int factorial(int n) {
 			
 			int result = 1;
 			if (n>12 || n<0) {
 				throw new IllegalArgumentException("Cannot use numbers bigger than 12 or smaller than zero");	
 			}
-
 			 for (int factor = 2; factor <= n; factor++) {
 			    result *= factor;
 			 }
-
-			  return result;
-	    
+			  return result;   
 		}
+		
+		/*
+		 * A method that checks if a number >=2 is a prime number
+		 */
+		public boolean isPrime(int n) {
+			if (n<2) {
+				throw new IllegalArgumentException("Number should be 2 or greater");	
+			}
+			if (n <= 1) 
+	            return false; 
+	  
+	        // Check from 2 to n-1 
+	        for (int i = 2; i < n; i++) 
+	            if (n % i == 0) 
+	                return false; 
+	  
+			return true;
+		}
+	
 }
 
 		

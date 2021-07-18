3e8561e20b2d1f7eff4edd0139f8d2d8ddf29fb0
elihuvillarausjmttol <miaoshun8077941573@163.com>
2021-07-18 19:19:41 +0800

    added the method findPrimesInFile and did the appropriate test case ( testFindPrimeNumbersInFileThatStartWith ) to cover the whole method.

--- seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/ArrayOperations.java
@@ -1,11 +1,36 @@
 package math;
 
+import io.FileIO;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class ArrayOperations consisting of a method called findPrimesInFile 
+ * that iterates in an array of numbers and returns a new array that 
+ * contains only prime numbers. Furthermore, the method uses FileIO to read a 
+ * file with numbers in the filepath location and the MyMath (to determine if a number is prime or not)
+ */
+
 public class ArrayOperations {
 	
 	
-		
-		
+	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
+		int[] numbers = fileIo.readFile(filepath);
+		List<Integer> PrimeNumbers = new ArrayList<>();
+
+		for (int num : numbers) {
+			if (myMath.isPrime(num)) {
+				PrimeNumbers.add(num);
+		    }
+
+		}
+			return PrimeNumbers.stream().mapToInt(i -> i).toArray();
+	
 	}
+		
+}
+	
+
 	
 	
 
